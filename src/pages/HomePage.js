641d89481be9e4f624b38d3bd18048729e1330d1
danialross <92746365+danialross@users.noreply.github.com>
2024-05-01 10:35:11 -0400

    loader function and refactors
    add loading spinner for when user is typing, refactor handlers into its own function for readability and maintainability, adjust selection height and width screen size, set min width for the page to not shrink ui

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,7 +2,7 @@ import Banner from "../components/Banner";
 import Column from "../components/Column";
 function HomePage() {
   return (
-    <div className="bg-dark-violet h-[880px]">
+    <div className="bg-dark-violet h-[880px] min-w-[1080px]">
       <Banner />
       <Column />
     </div>
