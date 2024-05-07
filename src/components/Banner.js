6f3d41c4e82eddefe74ff58c5433654030ca235f
danialross <92746365+danialross@users.noreply.github.com>
2024-05-06 20:51:31 -0400

    remove slogan and refactor
    remove unused code

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,9 +2,6 @@ function Banner({ type }) {
   return (
     <div className="w-full flex flex-col justify-center items-center bg-dark-violet pt-8 pb-8">
       <div className="text-6xl pt-4 text-sky-blue font-satisfy">VibeVault</div>
-      <div className="text-2xl pt-4 text-white font-nunito">
-        Uncover Music That Matches Your Beat
-      </div>
     </div>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -217,7 +217,7 @@ function Search() {
               <div role="status" className=" flex justify-center p-4">
                 <svg
                   aria-hidden="true"
-                  className="inline h-16 w-16 text-gray-200 animate-spin dark:text-gray-600 fill-purple-600 m-8"
+                  className="inline h-16 w-16 text-gray-600 animate-spin  fill-purple-600 m-8"
                   viewBox="0 0 100 101"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
